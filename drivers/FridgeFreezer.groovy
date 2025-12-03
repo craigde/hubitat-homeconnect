d48f857e7db1f075c557e9ff2b5a194a466a372c
craigde <66229848+craigde@users.noreply.github.com>
2025-12-03 12:38:02 -0800

    Update author and namespace in FridgeFreezer driver

--- drivers/FridgeFreezer.groovy
+++ drivers/FridgeFreezer.groovy
@@ -12,16 +12,14 @@
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
  *
- *  Home Connect FridgeFreezer (Child Device of Home Conection Integration)
+ *  Home Connect FridgeFreezer (Child Device of Home Connection Integration)
  *
- *  Author: Rangner Ferraz Guimaraes (rferrazguimaraes)
- *  Date: 2021-11-28
- *  Version: 1.0 - Initial commit
- *  Version: 1.1 - Removed unnecessary information for device
- *  Version: 1.2 - Fixed program errors
- *  Version: 1.3 - Added better handling of STOP events from event stream
- *  Version: 1.4 - Updating program when pressing 'Initialize' button
- *  Version: 1.5 - Added support for events DoorAlarmFreezer, DoorAlarmRefrigerator and TemperatureAlarmFreezer
+ *  Current owner: Craig Dewar (craigde)
+ *  Original author:Rangner Ferraz Guimaraes (rferrazguimaraes) for original driver port
+ *
+ *  Version history
+ *  1.0 - Initial commit
+ *
  */
 
 import groovy.transform.Field
@@ -34,7 +32,7 @@ import groovy.json.JsonSlurper
 def driverVer() { return "1.5" }
 
 metadata {
-    definition(name: "Home Connect FridgeFreezer", namespace: "rferrazguimaraes", author: "Rangner Ferraz Guimaraes") {
+    definition(name: "Home Connect FridgeFreezer", namespace: "craigde", author: "Craig Dewar") {
         capability "Sensor"
         //capability "Switch"
         capability "ContactSensor"
